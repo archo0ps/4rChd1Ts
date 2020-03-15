ae04f4131e3ff39667ac81da550b405950511a40
archo0ps <adm1n@live.com>
2020-03-15 07:10:53 -0400

    VERSION 0.1

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -5,11 +5,6 @@ import (
 )
 
 func TestDicFileToSlice(t *testing.T) {
-	v, _ := DicFileToSlice("../dic.txt", "http://192.168.61.215")
-	for i:=0;i<50;i++ {
-		t.Log(v[i])
-	}
-
 }
 
 func TestGenerateByUrl(t *testing.T) {
